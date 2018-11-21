3230eb231288e74f7a0611f9c6e7b97957d0db2f
paolov1928 <ventura.paolo@gmail.com>
2018-11-21 21:41:38 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,58 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  #index
+  # get '/' do
+  #   erb :index
+  # end
 
-  get '/' do
+  #index
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+  #new
+  get "/posts/new" do
+    erb :new
+    # binding.pry
+  end
+  #create
+  post "/posts" do
 
+    new_post = Post.create(name: params["name"], content: params["content"])
+    # redirect "/posts/#{new_post.id}"
+    redirect "/posts"
+  end
+
+  #show
+  #why do we have access to a params hash here?!
+  #need to do this after new otherwise will think that is an iD
+  get "/posts/:id" do
+    @selected_post = Post.find(params[:id])
+    erb :show
+  end
+
+  #edit
+
+  get '/posts/:id/edit' do
+    @selected_post_for_edit = Post.find(params[:id])
+    erb :edit
+  end
+
+  #update
+
+  patch "/posts/:id" do
+    selected_post_for_update = Post.find(params[:id])
+    selected_post_for_update.update(name: params["name"], content: params["content"])
+    redirect "/posts/#{selected_post_for_update.id}"
+  end
+
+  #delete
+
+  delete '/posts/:id/delete' do
+    selected_post_for_destruction = Post.find(params[:id])
+    selected_post_for_destruction.destroy
+    redirect "/posts"
+    #delete
   end
 end
